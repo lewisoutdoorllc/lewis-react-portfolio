5b833cff74a2ef6a67e69d397b9029866dbe8e7f
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-05-09 13:25:41 -0400

    added about me paragraph

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,19 +2,26 @@ import React from "react";
 import headShot from './img/head-shot.jpg';
 
 const About = () => (
-  <div className='p-3'> 
-    <img src= {headShot} className='head-shot' alt='' />
+  <div className='p-3'>
+    <img src={headShot} className='head-shot' alt='' />
     <h3 className='pl-1 pt-2 pb-4'>Cody Lewis</h3>
     <h1>A Little About Me</h1>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-      varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-      Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-      imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum ligula
-      in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare elit.
-      Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-      consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-      fames ac ante ipsum primis in faucibus.
+      My name is Cody Lewis and I am the proud founder and owner of Lewis Outdoor Solutions LLC based in Oviedo,
+      FL since 2010. I have over 20 years of small business management experience and have successfully grown my
+      current company into one of Central Florida’s most well-known and well-respected landscaping design and irrigation
+      businesses. My highly trained team of landscaping and irrigation professionals lead the industry in private,
+      commercial, and city related services. I offer a unique blend of leadership, vision, and knowledge and have served
+      throughout North Florida and Central Florida in all aspects of turf, lighting, irrigation, landscape, and consulting services.
+       </p>
+    <p>I currently on my way to becoming a full stack developer at UCF and will be updating this soon to say I am one.
+    Yay. I look forward to continuing my future in web development, application development, and would like to continue
+    into Cyber Security in the near future.
+      </p>
+    <p> I am the proud father of two beautiful children ages 2 and 8. My wife, Brooke, is also actively involved in
+    Lewis Outdoor Solutions as the COO and Human Resource Manager. We enjoy spending time together in the kitchen
+    (as a family) creating and making new and interesting meals. My family also enjoys four-wheeling, theme park hopping,
+    and spending time with our rescued Pit Bull, Annabelle.
     </p>
   </div>
 );
